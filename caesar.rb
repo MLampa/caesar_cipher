8b57688c8e1e6b16853addd326d25671d9b0767e
MLampa <meidenbee@gmail.com>
2016-02-22 09:36:18 -0500

    completed caesar cipher challenge

---
+++ caesar.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(string, num)
+  phrase = string.split("")
+  shift = (num.to_i)%26
+  phrase.each do |letter|
+    if letter =~ (/[a-zA-Z]/)
+      shift.times do
+        letter.next!
+      end
+    end
+    if letter.length > 1
+      letter[0] = ""
+    end
+  end
+  puts "'#{string}' shifted by #{num} becomes '#{phrase.join("")}'"
+end
+
+puts "Give me a word... any word."
+user = gets.chomp
+caesar_cipher(user, 2)
